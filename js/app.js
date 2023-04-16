5ce228e4320b4a0aedd44d7540d49516ff22ac57
rominaascurra <rominaascurra4887@gmail.com>
2023-04-16 18:16:59 -0300

    Validando que esten completos los campos del formulario

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let valorVenta = 0;
+const button = document.getElementById('boton')
 function precioVenta(precio){
     return precio - (precio*6.5/100)
 }
@@ -16,4 +17,32 @@ function conversion(cant){
     let equivalente = cant * valorVenta;
     document.getElementById('cantidadARS').value = `$ ${equivalente.toFixed(2)} ARS `
 }
+button.disabled = true;
+function habilitarBoton(){
+    let nombre = document.getElementById('nombreApellido').value;
+    let cuenta = document.getElementById('cuenta').value;
+    let cantidadUSD = document.getElementById('cantidadUSD').value;
+    let caja = document.getElementById('tipoCaja').value;
+    let des = 0;
+
+    if(nombre == ''){
+        des++;
+    }
+    if(cuenta.length < 6 || cuenta.length >= 23){
+        des++;
+    }
+    if(cantidadUSD == ''){
+        des++;
+    }
+    if(caja == '0'){
+        des++;
+    }
+   if(des===0){
+    button.disabled = false;
+   }else{
+    button.disabled = true;
+   }
+}
+document.getElementById('tipoCaja').addEventListener('change',habilitarBoton);
+document.getElementById('form').addEventListener('keyup',habilitarBoton);
 document.addEventListener('DOMContentLoaded', consultarApi());
